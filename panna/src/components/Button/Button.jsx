8136567a847d12a3de83d037b16f14378f24600d
Leozinho11 <leoferraribarreto@hotmail.com>
2022-07-27 19:31:28 -0300

    created idea

---
+++ panna/src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export function Button( {name} ) {
+    return(
+        <div className="first-button">
+            <button>{name}</button>
+        </div>
+    )
+}
---
+++ panna/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+
+
+
+function Home() {
+  return (
+    <div className="App">
+  
+    </div>
+  );
+}
+
+export default Home;
