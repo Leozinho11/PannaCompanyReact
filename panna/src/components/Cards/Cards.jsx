acdf0463ec1c73d8d320238f940a7e21eed58ef0
Leozinho11 <leoferraribarreto@hotmail.com>
2022-08-07 01:24:17 -0300

    Created Tables lobby

--- panna/src/components/Cards/Cards.jsx
+++ panna/src/components/Cards/Cards.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import './Cards.css';
+<<<<<<< Updated upstream
+=======
+import { Button } from '../Button/Button'
+import { Link } from 'react-router-dom'
+>>>>>>> Stashed changes
 
 
 export function Card({text, image, imageDescription, buttonName, directedTo}){
@@ -9,12 +14,19 @@ export function Card({text, image, imageDescription, buttonName, directedTo}){
     }
 
     return(
-        <div className="item">
+        <div className="card">
          <img src={image} alt= {imageDescription} />
+<<<<<<< Updated upstream
            <h3>{text}</h3> <br /> 
            <a href={directedTo}>
            <button onClick={handleChangePage}>{buttonName}</button>
            </a>
+=======
+           <h3>{text}</h3> <br />
+           <Link to= {directTo}>
+            <Button name={buttonName} />
+           </Link>
+>>>>>>> Stashed changes
         </div>
     )
 }
